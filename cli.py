b09a3ff5ed06ed5c3d92b9715b8879fb097dd6bd
AJXD2 <anthonyjohnkovach@gmail.com>
2023-09-09 17:20:50 -0400

    Added script functionanlity to cli.py like node.js

--- cli.py
+++ cli.py
@@ -1,4 +1,8 @@
+import enum
+import json
 from os import system
+from pathlib import Path
+import sys
 import fire
 from src import Main
 from src import explore_directory
@@ -38,9 +42,40 @@ class CLI(object):
         d = main.projects
 
         for i, item in enumerate(d):
-            print(
-                f"{i+1} [{item.category}/{item.identifier}] - {item.name} - {item.description:.10}"
-            )
+            if item.category == category:
+                print(
+                    f"{i+1} [{item.category}/{item.identifier}] - {item.name} - {item.description:.10}"
+                )
+
+    def run(self, script):
+        path = Path("./project_settings.json")
+
+        if path.exists():
+            with path.open("r") as f:
+                data = json.loads(f.read())
+
+            scripts_obj = data.get("scripts")
+            if scripts_obj is None:
+                print("Error: Project file error. No script object found!")
+                return
+
+            command = scripts_obj.get(script)
+
+            if command == None:
+                print(
+                    f"Error: No script with name '{script}' found in current config file!"
+                )
+                sys.exit()
+
+            if isinstance(command, list):
+                for index, cur in enumerate(command):
+                    print(f"Running command: '{cur}' ({index + 1}/{len(command)})")
+                    system(cur)
+                return
+            system(command)
+        else:
+            print("Error: No project file found in current directory!")
+            sys.exit()
 
 
 if __name__ == "__main__":
